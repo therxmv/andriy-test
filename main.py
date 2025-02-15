0ce8a192552ab43e843eb3d0e4609d1cd7e301ac
Andrii K <andriykorotchuk345@gmail.com>
2025-02-15 19:43:16 +0200

    Modify calculator

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from calculator import add, subtract
+from calculator import add, subtract, multiply, divide
 
 def main():
-   
+
     try:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
-        operation = input("Choose operation (+, -): ")
+        operation = input("Choose and operation: (+, -, *, /): ")
 
         if operation == "+":
             result = add(num1, num2)
         elif operation == "-":
             result = subtract(num1, num2)
+        elif operation == "*":
+            result = multiply(num1, num2)
+        elif operation == "/":
+            try:
+                result = divide(num1, num2)
+            except ValueError as e:
+                print(e)
+                return
         else:
-            print("Unknown operation!")
+            print("Unknown operation")
             return
 
-        print(f"Result: {result}")
+        print(f"Result {result}")
 
     except ValueError:
-        print("Uncorrect numbers!")
+        print("Uncorrect numbers")
 
-if name == "main":
+if __name__ == "__main__":
     main()
