dbbd4892f5f5006c52975aa9622c58519e7262c6
Roman Kuzmych <86236675+therxmv@users.noreply.github.com>
2025-02-15 19:29:08 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from calculator import add, subtract
+
+def main():
+   
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        operation = input("Choose operation (+, -): ")
+
+        if operation == "+":
+            result = add(num1, num2)
+        elif operation == "-":
+            result = subtract(num1, num2)
+        else:
+            print("Unknown operation!")
+            return
+
+        print(f"Result: {result}")
+
+    except ValueError:
+        print("Uncorrect numbers!")
+
+if name == "main":
+    main()
